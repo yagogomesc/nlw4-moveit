9f4509993e9dfa4ef305788a8f32f897022bcf7e
Yago Gomes <yagogomesc@gmail.com>
2021-02-26 02:36:31 -0300

    Adicionado CountdownProvider

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/yagogomesc.png" alt="Yago Gomes" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Yago Gomes</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
